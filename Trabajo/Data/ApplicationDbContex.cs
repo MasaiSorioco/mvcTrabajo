7d795a933e5fb642aa1cca1a919fb27a2a6a11c9
MasaiSorioco <manuelmasaisorioco@gmail.com>
2024-12-21 00:13:31 -0400

    primer commit

---
+++ Trabajo/Data/ApplicationDbContex.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Trabajo.Models;
+
+namespace Trabajo.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+    : base(options)
+        {
+        }
+        //Aqui todos los modelos que se creen 
+        public DbSet<Producto> Producto { get; set; }
+
+    }
+}
